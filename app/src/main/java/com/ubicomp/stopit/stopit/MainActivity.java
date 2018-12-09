d506e527324e8752cd412073a9aee78972ecf516
lokmane <lokmane.krizou@student.oulu.fi>
2018-12-09 13:05:18 +0200

    Created Canvas to draw Spiral from TouchScreen

--- app/src/main/java/com/ubicomp/stopit/stopit/MainActivity.java
+++ app/src/main/java/com/ubicomp/stopit/stopit/MainActivity.java
@@ -1,5 +1,8 @@
 package com.ubicomp.stopit.stopit;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DrawSpiralCanvas draw= new DrawSpiralCanvas(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            draw.setBackground(ContextCompat.getDrawable(this,R.drawable.spiral_draw));
+        }
+        setContentView(draw);
 
     }
 }
